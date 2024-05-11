0c2347921266af07584be17d2e683379deac4d59
surfu <krevsunaliona@gmail.com>
2024-05-11 16:59:38 +0300

    done

--- for/main.cpp
+++ for/main.cpp
@@ -1,84 +1,57 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int k, l;
+int main() {
     char j;
-    cout << "Hello World"
-         << "\n"
-         << "Choise one paramether"
-         << "\n"
-         << endl;
-    cout << "'s' - start programm\n'q' - close programm" << endl;
-    cin >> j;
-    if(j=='q'){
-        cout << "Programm closed" << endl;
-        return 0;
-    }else if(j=='s'){
-        for (;;){
-            cout << "Choise first wallet to convert: \nj - japanse yen\n u - ukraian hryvnya\n d - USA dollar" << endl;
-            cin >> l;
-            cout << "Choise second wallet to convert: \nj - japanse yen\n u - ukraian hryvnya\n d - USA dollar" << endl;
-            cin >> k;
-            switch (l)
-            {
-            case 'j':
-                if(k == l){
-                    cout << "It is a same wallet"<<endl;
-                    continue;
-                }else if(k == 'u'){
-                    cout << "You'll have: " << k * 0.25 << "hryvnyas";
-                    continue;
-                }else if(k == 'd'){
-                    cout << "You'll have: " << k * 0.01 << "dollars";
-                    continue;
-                }else{
-                    cout << "Error!"<<endl;
-                    continue;
-                }
-            case 'u':
-                if (k == l)
-                {
-                    cout << "It is a same wallet" << endl;
-                    continue;
-                }
-                else if (k == 'j')
-                {
-                    cout << "You'll have: " << k * 3.94 << "yens";
-                    continue;
-                }
-                else if (k == 'd')
-                {
-                    cout << "You'll have: " << k * 0.03 << "dollars";
-                    continue;
-                }
-                else
-                {
-                    cout << "Error!" << endl;
-                    continue;
-                }
-            case 'd':
-                if (k == l)
-                {
-                    cout << "It is a same wallet" << endl;
-                    continue;
-                }
-                else if (k == 'u')
-                {
-                    cout << "You'll have: " << k * 39.58 << "hryvnyas";
-                    continue;
-                }
-                else if (k == 'd')
-                {
-                    cout << "You'll have: " << k * 155.79 << "yens";
-                    continue;
-                }
-                else
-                {
-                    cout << "Error!" << endl;
-                    continue;
-                }
-            default:
+    double i;
+    for (;;) {
+        cout << "Hello World\nChoise one paramether\n\n";
+        cout << "'s' - start programm\n'q' - close programm\n";
+        cin >> j;
+
+        if (j == 'q') {
+            cout << "Programm closed\n";
             break;
-        }}
-}}
+        } else if (j == 's') {
+            char l, k;
+            cout << "Choise first wallet to convert: \nj - japanse yen\nu - ukraian hryvnya\nd - USA dollar\n";
+            cin >> l;
+            cout << "Choise second wallet to convert: \nj - japanse yen\nu - ukraian hryvnya\nd - USA dollar\n";
+            cin >> k;
+
+            cout << "Enter the amount to convert : ";
+            cin >>i;
+
+            if (l == k) {
+                cout << "It is the same wallet\n";
+                continue;
+            }
+
+            if (l == 'j') {
+                if (k == 'u')
+                    cout << "You'll have: " << i * 0.25 << " hryvnyas\n";
+                else if (k == 'd')
+                    cout << "You'll have: " << i * 0.01 << " dollars\n";
+                else
+                    cout << "Error!\n";
+            } else if (l == 'u') {
+                if (k == 'j')
+                    cout << "You'll have: " << i * 3.94 << " yens\n";
+                else if (k == 'd')
+                    cout << "You'll have: " << i * 0.03 << " dollars\n";
+                else
+                    cout << "Error!\n";
+            } else if (l == 'd') {
+                if (k == 'j')
+                    cout << "You'll have: " << i * 155.79 << " yens\n";
+                else if (k == 'u')
+                    cout << "You'll have: " << i * 39.58 << " hryvnyas\n";
+                else
+                    cout << "Error!\n";
+            } else {
+                cout << "Error!\n";
+            }
+        }
+    }
+    return 0;
+}
