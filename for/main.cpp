79fdde156cef612e8a37279e5888778b594f3146
surfu <krevsunaliona@gmail.com>
2024-05-16 15:40:38 +0300

    With if/else

--- for/main.cpp
+++ for/main.cpp
@@ -3,36 +3,41 @@ using namespace std;
 
 int main()
 {
-    int i;
-    char j;
-    for (;;)
-    {
-        cout << "n/y" << endl;
-        cin >> j;
-        if(j == 'n'){
+    int input_wallet, output_wallet, statik;
+    double jp = 155, uk = 40, us = 1;
+    char a, amount;
+    for (;;){
+        cout << "Do you want to continue programm? y/n" << endl;
+        cin >>a;
+        if(a = 'n'){
             break;
-        }else if (j =='y'){
-        cin >> i;
-        int hundr = i / 100;
-        cout << hundr;
-        int ten = (i / 10) % 10;
-        cout << ten;
-        int one = i % 10;
-        cout << one << endl;
-        if (hundr == 3 && hundr ==6)
-        {
-            cout << ten << one;
+        }else if(a = 'y'){
+            cout << "Enter your wallet (jp/uk/us): " << endl;
+            cin >> amount;
+            cout << "Enter the amount of money you want to convert: ";
+            cin >> input_wallet;
+            if(amount = 'jp'){
+                statik = input_wallet / 155;
+            }else if(amount = 'uk'){
+                statik = input_wallet / 40;
+            }else if(amount = 'us'){
+                statik = input_wallet / 1;
+            }else{
+                cout << "Wrong!" << endl;
+                continue;
+            }
+            cout << "Enter other wallet (jp/uk/us): " << endl;
+            cin >> a;
+            if(a == amount){
+                cout << "You can't convert to the same wallet!" << endl;
+            }else if(a == 'jp'){
+                output_wallet = statik * 155;
+            }else if(a=='uk'){
+                output_wallet = statik * 40;
+            }else if(a == 'us'){
+                output_wallet = statik * 1;
+            }
         }
-        else if(ten ==3 && ten ==6)
-        {
-            cout << hundr << one;
-        }
-        else if (one == 3 && one == 6)
-        {
-            cout << hundr << ten;
-        }
-    }else{
-        cout << "error" << endl;
-    }}
+    }
     return 0;
 }
