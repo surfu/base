b791cc067d52d2d5239a5905f00d065f490f1c67
surfu <krevsunaliona@gmail.com>
2024-05-18 13:07:15 +0300

    Done

--- for/main.cpp
+++ for/main.cpp
@@ -3,40 +3,62 @@ using namespace std;
 
 int main()
 {
-    int input_wallet, output_wallet, statik;
-    double jp = 155, uk = 40, us = 1;
-    char a, amount;
-    for (;;){
+    int input_wallet, output_wallet, amount;
+    double jp = 155, uk = 40, us = 1, statik;
+    char a, b;
+    for (;;)
+    {
         cout << "Do you want to continue programm? y/n" << endl;
-        cin >>a;
-        if(a = 'n'){
+        cin >> a;
+        if (a == 'n')
+        {
             break;
-        }else if(a = 'y'){
-            cout << "Enter your wallet (jp/uk/us): " << endl;
+        }
+        else if (a == 'y')
+        {
+            cout << "Enter your wallet (jp - 1/uk - 2/us - 3): " << endl;
             cin >> amount;
             cout << "Enter the amount of money you want to convert: ";
             cin >> input_wallet;
-            if(amount = 'jp'){
-                statik = input_wallet / 155;
-            }else if(amount = 'uk'){
-                statik = input_wallet / 40;
-            }else if(amount = 'us'){
-                statik = input_wallet / 1;
+            if (amount == 1)
+            {
+                statik = input_wallet / jp;
+            }
+            else if (amount == 2)
+            {
+                statik = input_wallet / uk;
+            }
+            else if (amount == 3)
+            {
+                statik = input_wallet / us;
+            }
+            else
+            {
+                cout << "Wrong!" << endl;
+                continue;
+            }
+            cout << "Enter wallet to convert (jp - 1/uk - 2/us - 3): " << endl;
+            cin >> b;
+            if (b == amount)
+            {
+                cout << "You can't convert to the same wallet!" << endl;
+            }
+            else if (b == '1')
+            {
+                output_wallet = statik * jp;
+            }
+            else if (b == '2')
+            {
+                output_wallet = statik * uk;
+            }
+            else if (b == '3')
+            {
+                output_wallet = statik * us;
             }else{
                 cout << "Wrong!" << endl;
                 continue;
             }
-            cout << "Enter other wallet (jp/uk/us): " << endl;
-            cin >> a;
-            if(a == amount){
-                cout << "You can't convert to the same wallet!" << endl;
-            }else if(a == 'jp'){
-                output_wallet = statik * 155;
-            }else if(a=='uk'){
-                output_wallet = statik * 40;
-            }else if(a == 'us'){
-                output_wallet = statik * 1;
-            }
+            cout << "You'll have: " << output_wallet << endl;
         }
     }
     return 0;
