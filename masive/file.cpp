38b30d6fdd69675bb208402ec406925fba587c3c
surfu <krevsunaliona@gmail.com>
2024-05-18 15:43:35 +0300

    first version test

--- masive/file.cpp
+++ masive/file.cpp
@@ -4,5 +4,18 @@ using namespace std;
 int main()
 {
     const int size = 6;
-    int arr[size];
+    int arr[size], num;
+
+    for (;;){
+        for (int i = 0; i < 6;i++){
+            cout << "Enter a sum on a month: ";
+            cin >> num;
+            arr[i] = num;
+        }
+        for (int i = 0; i < size;i++){
+            cout << arr[i]<< endl;
+        }
+
+    }
+    return 0;
 }
