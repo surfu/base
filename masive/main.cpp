ec6ba4a273e694303131032a4e4019ecf8e36014
surfu <krevsunaliona@gmail.com>
2024-05-30 19:52:10 +0300

    done

--- masive/main.cpp
+++ masive/main.cpp
@@ -5,10 +5,11 @@ using namespace std;
 int main()
 {
     srand(time(0));
-    int a, b, c;
+    int a, b, c=0;
     cout << "Enter two numbers: ";
     cin >> a >> b;
     int arr[a][b];
+
     for (int i = 0; i < a;i++){
         for (int j = 0; j < b;j++){
             arr[i][j] = rand() % 10;
@@ -22,5 +23,40 @@ int main()
         }
         cout << endl;
     }
-        return 0;
+
+    // 1
+    for (int i = 0; i < a;i++){
+        for (int j = 0; j < b;j++){
+            c += arr[i][j];
+        }
+    }
+    cout << c;
+    // 2
+    c = c / (a * b);
+    cout << c;
+    // 4
+    c = 0;
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < b; j++)
+        {
+            if(arr[i][j]>c){
+                c = arr[i][j];
+            }
+        }
+    }
+    cout << c;
+    // 3
+    for (int i = 1; i < a; i++)
+    {
+        for (int j = 1; j < b; j++)
+        {
+            if (arr[i][j] < arr[i-1][j-1])
+            {
+                c = arr[i][j];
+            }
+        }
+    }
+    cout << c;
+    return 0;
 }
