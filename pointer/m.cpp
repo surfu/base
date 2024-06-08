fa95959af43c8a5931953853013c0b60b511ac34
surfu <krevsunaliona@gmail.com>
2024-06-08 17:15:30 +0300

    done

---
+++ pointer/m.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int *size;
+    int *sum = 0;
+    int *ptr;
+    int arr[*size];
+
+    for(int i =0; i<*size;++i){
+        *ptr += rand() % 100;
+        *ptr = arr[i];
+    }
+
+    for (int i = 0; i < *size; ++i) {
+        sum += *(ptr + i);
+    }
+
+    cout << "Sum of array elements: " << sum << endl;
+
+    return 0;
+}
