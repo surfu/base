a2e7702b7fa0afbdec2ae2fa342b7daac7792d72
surfu <krevsunaliona@gmail.com>
2024-06-08 16:37:29 +0300

    done

--- pointer/m.cpp
+++ pointer/m.cpp
@@ -3,22 +3,21 @@
 using namespace std;
 
 int main() {
-    int num;
     
     cout << "Enter a num: ";
-    cin >> num;
+    int* ptrnum =  new int;
+    cin >> *ptrnum;
 
-    int* ptrnum = &num;
 
-    (*ptrnum>0)? cout << "The num is positive." << endl : (*ptrnum<0)? cout << "The num is negative." << endl : cout << "The num is zero." << endl;
+    // (*ptrnum>0)? cout << "The num is positive." << endl : (*ptrnum<0)? cout << "The num is negative." << endl : cout << "The num is zero." << endl;
 
-    // if (*ptrnum > 0) {
-    //     cout << "The num is positive." << endl;
-    // } else if (*ptrnum < 0) {
-    //     cout << "The num is negative." << endl;
-    // } else {
-    //     cout << "The num is zero." << endl;
-    // }
+    if (*ptrnum > 0) {
+        cout << "The num is positive." << endl;
+    } else if (*ptrnum < 0) {
+        cout << "The num is negative." << endl;
+    } else {
+        cout << "The num is zero." << endl;
+    }
 
     return 0;
 }
