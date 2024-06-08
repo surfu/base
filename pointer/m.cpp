cd841c5fdee19047005557812a69bef0a1b9654b
surfu <krevsunaliona@gmail.com>
2024-06-08 16:52:34 +0300

    done

---
+++ pointer/m.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int* ptrnum = new int;
+    int* ptrnum1= new int;
+    int* ptropt= new int;
+    cout << "Enter a num: ";
+    cin>> *ptrnum;
+    cout << "Enter a other num: ";
+    cin>>*ptrnum1;
+    
+    *ptropt = *ptrnum;
+    *ptrnum = *ptrnum1;
+    *ptrnum1 = *ptropt;
+
+    cout<<*ptrnum<<*ptrnum1<<endl;
+
+    delete ptrnum;
+    delete ptrnum1;
+    delete ptropt;
+
+    return 0;
+}
