3350fe48633edcc715d4fea3ba99cbc8b791d37e
surfu <krevsunaliona@gmail.com>
2024-06-22 16:32:26 +0300

    done

---
+++ str/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void def(char str[])
+{
+    int words = 0, num = 0, sym = 0, ath = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (int(str[i]) < 123 && int(str[i]) > 64)
+        {
+            words++;
+        }
+        else if (int(str[i]) < 58 && int(str[i]) > 47)
+        {
+            num++;
+        }
+        else if (int(str[i]) < 47 && int(str[i]) > 43 || int(str[i]) == 33)
+        {
+            sym++;
+        }
+        else
+        {
+            ath++;
+        }
+    }
+    cout << words << " | " << num << " | " << sym << endl;
+}
+
+int main()
+{
+    char str1[] = "Hell4o world!";
+    def(str1);
+    cout << str1 << endl;
+}
