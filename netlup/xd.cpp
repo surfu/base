75e47e1b12f808e7a369c593cf68115a345e20f3
surfu <krevsunaliona@gmail.com>
2024-07-01 13:31:10 +0300

    done

--- netlup/xd.cpp
+++ netlup/xd.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 bool isPrime(int num) {
     if (num <= 1) return false;
-    for (int i = 2; i <= sqrt(num); ++i) {
+    if (num == 2) return true;
+    if (num % 2 == 0) return false;
+    for (int i = 3; i * i <= num; i += 2) {
         if (num % i == 0) return false;
     }
     return true;
